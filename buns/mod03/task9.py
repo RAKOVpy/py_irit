e0a435354510940aa74ea3f2ed3aff038fcc3d1c
RAKOVpy <dima.rakov.05@mail.ru>
2023-10-11 12:37:31 +0500

    hw2_upd_2

--- buns/mod03/task9.py
+++ buns/mod03/task9.py
@@ -1,11 +1,10 @@
 N = int(open('input9.txt').readline())
-len_ans, i = 0, 0
+len_ans = 0
 ans = [0, 0]
-while True:
+for i in range(10 ** 6):
     for _ in range((i + 2) // 2):
         if len_ans == N:
             open('output9.txt', mode='w').write(f'{ans[0]} {ans[1]}')
             exit()
         ans = [ans[0] - (i % 4 == 0) + (i % 4 == 2), ans[1] - (i % 4 == 1) + (i % 4 == 3)]
         len_ans += 1
-    i += 1
